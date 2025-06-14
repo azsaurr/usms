3918a731b77f9d5fb1827c9715a45ecb6762cc3f
AZ <102905929+azsaurr@users.noreply.github.com>
2025-06-14 12:34:15 +0800

    chore(dev): rewrite template dev environment flake.nix with more project specific configuration using uv2nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,89 @@
+
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  description = "USMS Development Environment Flake";
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
-      in {
-        default = mkPoetryApplication { projectDir = self; };
-      });
+  inputs = {
+    systems.url = "github:nix-systems/default";
 
-      devShells = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
-      in {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            poetry
-            uv
-          ];
-        };
-      });
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = {
+    self,
+    systems,
+    nixpkgs,
+    uv2nix,
+    ...
+  }:
+  let
+    inherit (nixpkgs) lib;
+    forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f { pkgs = import nixpkgs { inherit system; }; });
+  in {
+    devShells = forEachSystem (
+      { pkgs }: {
+        default =
+        let
+          # Use Python 3.12 from nixpkgs
+          python = pkgs.python312;
+
+        in pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            # Set packages here
+            git
+            python
+            uv
+
+            # Set python packages here
+            (python.withPackages (ps: with ps; [
+              
+            ]))
+          ];
+
+          env = {
+            # Prevent uv from managing Python downloads
+            #UV_PYTHON_DOWNLOADS = "never";
+
+            # Force uv to use nixpkgs Python interpreter
+            #UV_PYTHON = python.interpreter;
+
+            # OPTIONAL: set username and password as environment variables here
+            #USMS_USERNAME = "username";
+            #USMS_PASSWORD = "password";
+          }
+          # IMPORTANT!
+          # Fixes numpy and pandas
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Python libraries often load native shared objects using dlopen(3).
+            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+          # Additionally, use nix-ld by including the following in nixos config:
+          # programs.nix-ld = {
+          #   enable = true;
+          #   libraries = options.programs.nix-ld.libraries.default;
+          # };
+          # Fixes ruff
+
+          # Shell commands to run on activation
+          shellHook = ''
+            # Create and activate virtual environment
+            uv venv
+            source .venv/bin/activate
+            
+            # Install all project + dev dependencies
+            uv sync --all-extras
+
+            # Install the pre-commit hooks
+            pre-commit install --install-hooks
+          '';
+          };
+
+      }
+    );
+  };
 }
